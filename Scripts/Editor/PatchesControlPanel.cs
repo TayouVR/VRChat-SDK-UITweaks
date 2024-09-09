64cfd0bd573b066774f9b64ca15a773f10fd4d3f
Tayou <git@tayou.org>
2024-09-09 22:55:18 +0200

    comment out the patches that don't work
    maybe i can revisit them later...

--- Scripts/Editor/PatchesControlPanel.cs
+++ Scripts/Editor/PatchesControlPanel.cs
@@ -10,43 +10,43 @@ namespace Tayou.VRChat.SDKUITweaks.Editor {
         // This doesn't work in SDK versions 3.3.0 and higher, I have no idea how to fix right now.
         // ******************
 
-        [HarmonyPriority(Priority.Low)]
-        private class PatchControlPanelErrorsOrIssues : PatchBase {
-            
-            public override string GetPatchDisplayName() {
-                return "Disable Control Panel Error Blocking 1";
-            }
-
-            protected override IEnumerable<MethodBase> GetPatches() {
-                yield return AccessTools.Method(typeof(VRCSdkControlPanel), nameof(VRCSdkControlPanel.NoGuiErrorsOrIssues));
-            }
-            
-            // ReSharper disable once InconsistentNaming
-            public static bool Prefix(VRCSdkControlPanel __instance, ref bool __result) {
-                __result = true;
-                //DebugLog("The patch is working 1");
-                return false;
-            }
-        }
-        
-        [HarmonyPriority(Priority.Low)]
-        private class PatchControlPanelErrors : PatchBase {
-            
-            public override string GetPatchDisplayName() {
-                return "Disable Control Panel Error Blocking 2";
-            }
-
-            protected override IEnumerable<MethodBase> GetPatches() {
-                yield return AccessTools.Method(typeof(VRCSdkControlPanel), nameof(VRCSdkControlPanel.NoGuiErrors));
-            }
-
-            // ReSharper disable once InconsistentNaming
-            public static bool Prefix(VRCSdkControlPanel __instance, ref bool __result) {
-                __result = true;
-                //DebugLog("The patch is working 2");
-                return false;
-            }
-        }
+        // [HarmonyPriority(Priority.Low)]
+        // private class PatchControlPanelErrorsOrIssues : PatchBase {
+        //     
+        //     public override string GetPatchDisplayName() {
+        //         return "Disable Control Panel Error Blocking 1";
+        //     }
+        //
+        //     protected override IEnumerable<MethodBase> GetPatches() {
+        //         yield return AccessTools.Method(typeof(VRCSdkControlPanel), nameof(VRCSdkControlPanel.NoGuiErrorsOrIssues));
+        //     }
+        //     
+        //     // ReSharper disable once InconsistentNaming
+        //     public static bool Prefix(VRCSdkControlPanel __instance, ref bool __result) {
+        //         __result = true;
+        //         //DebugLog("The patch is working 1");
+        //         return false;
+        //     }
+        // }
+        //
+        // [HarmonyPriority(Priority.Low)]
+        // private class PatchControlPanelErrors : PatchBase {
+        //     
+        //     public override string GetPatchDisplayName() {
+        //         return "Disable Control Panel Error Blocking 2";
+        //     }
+        //
+        //     protected override IEnumerable<MethodBase> GetPatches() {
+        //         yield return AccessTools.Method(typeof(VRCSdkControlPanel), nameof(VRCSdkControlPanel.NoGuiErrors));
+        //     }
+        //
+        //     // ReSharper disable once InconsistentNaming
+        //     public static bool Prefix(VRCSdkControlPanel __instance, ref bool __result) {
+        //         __result = true;
+        //         //DebugLog("The patch is working 2");
+        //         return false;
+        //     }
+        // }
 
     }
 
